a5f7321b3925684c620baffc25b6feae974e58e7
Danilo <danilo.feitosa94@hotmail.com>
2024-09-04 20:40:24 -0300

    Recuperacao UC 12 e 13

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using recuperacao_lhpets;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Projeto Web LH Pets - Versão 1.0");
+
+Banco banco = new Banco();
+
+app.MapGet("/listaClientes", (HttpContext context) => {
+    context.Response.WriteAsync(banco.GetListaString());
+});
+
+app.UseStaticFiles();
+
+app.Run();
